1466476f11442b7bf7b80c7755ba1bec3a0d90f0
ysdart <70014133+ysdart@users.noreply.github.com>
2022-03-27 17:48:34 +0900

    わからんけど

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./styles.css";
+
+//テキストボックスの値を取得し、初期化する
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  alert(inputText);
+};
+
+//追加ボタンクリックでonClickAdd発生
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
