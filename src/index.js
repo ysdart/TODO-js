a9e4f76e6817bdef84eed34e5bd78455d75a125a
ysdart <70014133+ysdart@users.noreply.github.com>
2022-03-31 00:32:00 +0900

    未完了周りbutton作成

--- src/index.js
+++ src/index.js
@@ -5,19 +5,35 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
-  //div生成
-  const div = document.createElement("div");
-  div.className = "list-row";
-
   //liタグ生成
   const li = document.createElement("li");
-  li.innerText = inputText;
+  li.className = "list-row";
 
-  //divタグの子要素に各要素を設定
-  div.appendChild(li);
+  //pタグ作成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  //buttopn(完了)タグ作成
+  const completebutton = document.createElement("button");
+  completebutton.innerText = "完了";
+  completebutton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //buttopn(削除)タグ作成
+  const deletebutton = document.createElement("button");
+  deletebutton.innerText = "削除";
+  deletebutton.addEventListener("click", () => {
+    alert("削除");
+  });
+
+  //liタグの子要素に各要素を設定
+  li.appendChild(p);
+  li.appendChild(completebutton);
+  li.appendChild(deletebutton);
 
   //未完了リストに追加
-  document.getElementById("incomplete-list").appendChild(div);
+  document.getElementById("incomplete-list").appendChild(li);
 };
 
 //追加ボタンクリックでonClickAdd発生
